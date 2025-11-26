787d2668ce078b0094078af40ef15c30e2112606
LCF2370 <c0921675@mylambton.ca>
2025-11-25 19:34:56 -0500

    Ralph: Able to display without the errors. Plans to install bootstrap using node package management.

--- client/src/components/Catalogue.jsx
+++ client/src/components/Catalogue.jsx
@@ -1,34 +1,39 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-const Catalogue = (props) => (
-    <tr>
-        <td>props.catalaogue.name</td>
+const Catalogue = ({catalogue}) => (
+    <tr key={catalogue.id}>
+        <td>{catalogue.name}</td>
+        <td>{catalogue.category}</td>
+        <td>{catalogue.servicedBy}</td>
+        <td>{catalogue.price}</td>
+        <td>{catalogue.availability}</td>
+        <td>{catalogue.description}</td>
     </tr>
 );
 
-export default function Catalogue(){
+export default function ServiceCatalogue() {
     const [catalogues, setCatalogues] = useState([]);
-    
+
     useEffect(() => {
         async function getCatalogues() {
-            const response = await fetch(`http://localhost:3000/`);
+            const response = await fetch(`http://localhost:3000/v1/services`);
             if(!response.ok) {
                 const message =`An error occured: ${response.statusText}`;
                 console.error(message);
                 return;
             }
-            const catalogues = await response.json();
-            setCatalogues(catalogues);
+            const list = await response.json();
+            setCatalogues(list.service);
         }
         getCatalogues();
         return;
-    }, [catalogues,lenght]);
+    }, [catalogues.lenght]);
 
     function serviceCatalogues() {
         return catalogues.map((catalogue) => {
             return (
-                <Catalogue catalogue={catalogue} key={catalogue._id}/>
+                <Catalogue catalogue={catalogue} key={catalogue.id}/>
             );
         });
     }
@@ -41,6 +46,11 @@ export default function Catalogue(){
                     <thead>
                         <tr>
                             <th>Name</th>
+                            <th>Category</th>
+                            <th>Staff</th>
+                            <th>Service Price</th>
+                            <th>Status</th>
+                            <th>Description</th>
                         </tr>
                     </thead>
                     <tbody>
