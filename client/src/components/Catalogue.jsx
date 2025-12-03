b49b8604a972df4dc4a463cc17ffc8adb4c00109
LCF2370 <c0921675@mylambton.ca>
2025-12-02 22:07:02 -0500

    Ralph: Added two anchor tag button for delete and edit for future functions. Created a function to indicate the service is  unavailable or available. Added text for price of the service. Change color for avail service to green. Change color for staff available to light blue. Colored the edit to blue. Colored the delete/remove to red. Used text truncation for service description.

--- client/src/components/Catalogue.jsx
+++ client/src/components/Catalogue.jsx
@@ -4,20 +4,33 @@ import { Link } from "react-router-dom";
 const Catalogue = ({catalogue}) => (
     <div className="col-3">
         <div className="card m-1" key={catalogue.id}>
+            <div className="text-end">
+                <a href="#" className="btn text-primary m-1 p-0"><i class="fa-solid fa-pencil"></i></a>
+                <a href="#" className="btn text-danger me-1 p-0"><i class="fa-solid fa-trash-can"></i></a>
+            </div>
             <div className="card-header text-center">
                 <h3><i class="fa-regular fa-hand"></i></h3>
                 <h4 className="strong">{catalogue.category}</h4>
-                <h6 className="text-warning"><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-regular fa-star"></i></h6>
+                <h3 className="text-primary">₱{catalogue.price}</h3>
             </div>
             <div className="card-body">
-                <h5 className="card-title text-center">{catalogue.name}</h5>
-                <p className="card-text">{catalogue.description}</p>
-                <a href="#" className="btn btn-primary"><i class="fa-solid fa-plus"></i></a> &nbsp;
-                <a href="#" className="btn btn-primary"><i class="fa-solid fa-address-card"></i></a>
+                {serviceStatus(catalogue.availability, catalogue.name)}
+                <h6 className="text-warning"><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-regular fa-star"></i></h6>
+                <p className="card-text text-truncate">{catalogue.description}</p>
+                <a href="#" className="btn btn-success"><i class="fa-solid fa-cart-plus"></i></a> &nbsp;
+                <a href="#" className="btn btn-info"><i class="fa-solid fa-address-card"></i></a>
             </div>
         </div>
     </div>
 );
+
+function serviceStatus(status, name) {
+    if (status !== "Available") {
+        return <h5 className="card-title text-center text-danger lead text-decoration-line-through">{name}</h5>;
+    } else {
+        return <h5 className="card-title text-center lead">{name}</h5>;
+    }
+}
 
 export default function ServiceCatalogue() {
     const [catalogues, setCatalogues] = useState([]);
