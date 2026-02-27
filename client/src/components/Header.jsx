f37dee9fdc91e505fca75b16a9a3eb6530a94aa2
LCF2370 <c0921675@mylambton.ca>
2026-02-27 15:00:03 -0500

    Ralph: Added a new dummy page for the mean time for future improvement Events and Contact. Home Nav button for landing Page. Service Nav Button for catalogues of services offered by the company. PPAC will replaced by a company logo soon.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,26 +1,32 @@
 const Header = () => {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-            <a className="navbar-brand text-warning" href="/">SCM</a>
+        <div className="container-fluid hstack gap-3">
+            <a className="navbar-brand text-warning w-75" href="/">PPAC</a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
             </button>
-            <div className="collapse navbar-collapse" id="navbarNavDropdown">
+            <div className="collapse navbar-collapse ms-auto" id="navbarNavDropdown">
             <ul className="navbar-nav">
-                <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/service">Catalogue</a>
+                <li className="nav-item ">
+                    <a className="nav-link" aria-current="page" href="/">Home</a>
                 </li>
-                <li className="nav-item">
-                <a className="nav-link" href="#" hidden>Promotions</a>
+                <li className="nav-item ">
+                    <a className="nav-link" aria-current="page" href="/service">Service</a>
                 </li>
-                <li className="nav-item">
-                <a className="nav-link" href="#" hidden>Pricing</a>
+                <li className="nav-item ">
+                    <a className="nav-link" href="#" hidden>Promo</a>
+                </li>
+                <li className="nav-item ">
+                    <a className="nav-link" href="#">Events</a>
+                </li>
+                <li className="nav-item ">
+                    <a className="nav-link" href="#">Contact</a>
                 </li>
                 <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" hidden>
-                    Dropdown link
-                </a>
+                    <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" hidden>
+                        Dropdown link
+                    </a>
                 <ul className="dropdown-menu">
                     <li><a className="dropdown-item" href="#">Action</a></li>
                     <li><a className="dropdown-item" href="#">Another action</a></li>
