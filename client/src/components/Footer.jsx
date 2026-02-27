c6c029544929984f47f5145aefe583e5a6db7297
LCF2370 <c0921675@mylambton.ca>
2026-02-27 15:24:27 -0500

    Ralph: Structured footer as my basis for future improvements.

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,8 +1,42 @@
 const Footer = () => {
   return (
-    <div>
-      <h3>Footer</h3>
-    </div>
+    <>
+      <div className="text-bg-dark vw-100 m-0 mb-2 p-1 w-100 hstack">
+        <div className="p-2 w-50">
+          <p>
+            Patricia's Prettylash and Aesthetic Clinic <br />
+            Balingasag, Misamis Oriental, Philippines <br />
+            patriciaprettylash@gmail.com <br />
+            +63 (965)-926-6385 <br />
+          </p>
+        </div>
+        <div className="vr"></div>
+        <div className="p-2 w-25">
+          <p>
+            instagram <br />
+            facebook <br />
+            whatsapp <br />
+            linked-in <br />
+          </p>
+        </div>
+        <div className="vr"></div>
+        <div className="p-2 w-25">
+          <p>
+            Career <br />
+            Review <br />
+            FAQ <br />
+            Help <br />
+          </p>
+        </div>
+      </div>
+      <div className="hr"></div>
+      <div className="bg-light w-100 text-center m-0 p-1">
+        <p>
+          Powered by <br />
+          &#169; 2026 REBORN GEEK&reg;
+        </p>
+      </div>
+    </>
   );
 };
 
